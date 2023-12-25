e6c02f10bcfb653e3caebf0d6ff9049600927bd1
Admin <purohitparthiv@gmail.com>
2023-12-25 21:10:41 +0530

    first

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,6 +7,9 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
 
     }
 
@@ -14,17 +17,78 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if(sum<0 || sum > 9*digits)
+        {
+            return null;
+        }
+        int minsum = sum;
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<digits-1;i++)
+        {
 
-        return null;
+            int digit = Math.min(minsum,9);
+            if(digit<0)
+            {
+                throw new InvalidAccountNumberException("Account number can not be generated");
+            }
+            else
+            {
+                ans.append(digit);
+                minsum = minsum - digit;
+            }
+        }
+        ans.append(minsum);
+
+        return ans.toString();
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance = this.balance + amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        if(amount > this.balance)
+        {
+            throw new insufficientBalance("Insufficient Balance");
+        }
+        else
+        {
+            if(this.balance - amount < this.minBalance)
+            {
+                throw new insufficientBalance("Insufficient Balance");
+            }
+            this.balance = this.balance - amount;
+
+
+        }
+
+    }
+    public static class InvalidAccountNumberException extends Exception
+    {
+        InvalidAccountNumberException(String msg)
+        {
+            super(msg);
+        }
+
+    }
+    public double getBalance()
+    {
+        return this.getBalance();
+    }
+
+    public void setBalance(double temp)
+    {
+        this.balance = temp;
+    }
+    public static class insufficientBalance extends Exception
+    {
+        insufficientBalance(String msg)
+        {
+            super(msg);
+        }
 
     }
 
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,6 +5,15 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+        if(balance < 5000)
+        {
+            throw new insufficientBalance("Insufficient Balance");
+        }
+        else
+        {
+            this.tradeLicenseId = tradeLicenseId;
+        }
 
     }
 
@@ -13,6 +22,64 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        if(this.tradeLicenseId == null )
+        {
+            throw new invalidLicense("Valid License can not be generated");
+        }
+        if(this.tradeLicenseId.length()<2)
+        {
+            return;
+        }
+        char[] chars = this.tradeLicenseId.toCharArray();
+        for(int i=1;i<chars.length;i++)
+        {
+            if(chars[i]==chars[i-1])
+            {
+                rearrange(chars,i);
+            }
+        }
+        if(check(chars))
+        {
+            throw new invalidLicense("Valid License can not be generated");
+        }
+        else
+        {
+            this.tradeLicenseId = chars.toString();
+        }
+
+    }
+    public boolean check(char[] chars)
+    {
+        for(int i=1;i<chars.length;i++)
+        {
+            if(chars[i]==chars[i-1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void rearrange(char[] chars,int index)
+    {
+        char temp = chars[index];
+        chars[index] = chars[chars.length-index];
+        chars[chars.length-index] = temp;
+
+    }
+    public static class insufficientBalance extends Exception
+    {
+        insufficientBalance(String msg)
+        {
+            super(msg);
+        }
+
+    }
+    public static class invalidLicense extends Exception
+    {
+        invalidLicense(String msg)
+        {
+            super(msg);
+        }
 
     }
 
